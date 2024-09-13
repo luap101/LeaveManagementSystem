a574d8422acfc1ce4d412d23262c2fb0b2842b12
Davis <Davis@DESKTOP-77E92AR>
2024-09-12 19:23:42 -0500

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            { Name = "Student of MVC",
+             DateofBirth = new DateTime(2020,12,23)
+            };
+            return View(data);
+        }
+    }
+}
